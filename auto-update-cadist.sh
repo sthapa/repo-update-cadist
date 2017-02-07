3d53cdeaa28af06adac32cbbd9709c10ebf2a9d6
Matyas Selmeci <matyas@cs.wisc.edu>
2017-02-07 15:58:15 -0600

    Fix indent

--- auto-update-cadist.sh
+++ auto-update-cadist.sh
@@ -44,116 +44,116 @@ wget -q "$OSG_SECURITY_PUBKEY_URL" -O "$GPG_HOME/$OSG_SECURITY_PUBKEY"
 gpg_wrapper --import "$GPG_HOME/$OSG_SECURITY_PUBKEY"
 
 for TYPES in NEW IGTFNEW; do
-        SUFFIX=$TYPES
-        case ${TYPES} in
-            IGTFNEW)
-                SYMEXT="igtf-new"
-                FILEEXT="-igtf-new"
-                CURRDIR="igtf-new"
-                RPM="igtf-ca-certs"
-                ;;
-            NEW)
-                SYMEXT="new"
-                FILEEXT="-new"
-                CURRDIR="new"
-                RPM="osg-ca-certs"
-                ;;
-            *)
-                message "Bad thing, if this happens something is really wrong"
-                exit 1
-                ;;
-        esac
-
-        downloaddir=$(mktemp -d)
-        pushd $downloaddir >/dev/null
-        yumdownloader --source $RPM >/dev/null
-        rpmfile=$(/bin/ls *.src.rpm)
-        if [[ ! -f $rpmfile ]]; then
-            message "$RPM: unable to download from repos"
+    SUFFIX=$TYPES
+    case ${TYPES} in
+        IGTFNEW)
+            SYMEXT="igtf-new"
+            FILEEXT="-igtf-new"
+            CURRDIR="igtf-new"
+            RPM="igtf-ca-certs"
+            ;;
+        NEW)
+            SYMEXT="new"
+            FILEEXT="-new"
+            CURRDIR="new"
+            RPM="osg-ca-certs"
+            ;;
+        *)
+            message "Bad thing, if this happens something is really wrong"
             exit 1
-        fi
-        rpm2cpio $rpmfile | cpio --quiet -id '*.tar.gz'
-        TARBALL=$(/bin/ls *.tar.gz)
-        if [[ ! -f $TARBALL ]]; then
-            message "$rpmfile: couldn't extract tarball"
-            exit 1
-        fi
-        if ! echo "$TARBALL" | grep -Eq "^osg-certificates-[[:digit:]]+\.[[:digit:]]+${SUFFIX}.tar.gz$"; then
-            message "$TARBALL: bad tarball name"
-            message "Extracted from $rpmfile"
-            exit 1
-        fi
-        v=${TARBALL%${SUFFIX}.tar.gz}
-        VERSION_CA=${v#osg-certificates-}
-        SIGFILE=${TARBALL}.sig
-        SIGFILE_URL=${CADISTREPO}/${CADISTREPORELEASETYPE}/${SIGFILE}
-        if ! svn export -q --force "$SIGFILE_URL" "$SIGFILE"; then
-            message "$SIGFILE: unable to download"
-            message "Upstream URL: $SIGFILE_URL"
-            exit 1
-        fi
-        if ! gpg_wrapper --verify "$SIGFILE"; then
-            message "$tarball: GPG verification failed"
-            exit 1
-        fi
+            ;;
+    esac
 
-        CADIR="${TMP}/cadist/${VERSION_CA}${SUFFIX}"
-        CATARBALL="${CADIR}/$TARBALL"
-        CASIGFILE="${CADIR}/$SIGFILE"
+    downloaddir=$(mktemp -d)
+    pushd $downloaddir >/dev/null
+    yumdownloader --source $RPM >/dev/null
+    rpmfile=$(/bin/ls *.src.rpm)
+    if [[ ! -f $rpmfile ]]; then
+        message "$RPM: unable to download from repos"
+        exit 1
+    fi
+    rpm2cpio $rpmfile | cpio --quiet -id '*.tar.gz'
+    TARBALL=$(/bin/ls *.tar.gz)
+    if [[ ! -f $TARBALL ]]; then
+        message "$rpmfile: couldn't extract tarball"
+        exit 1
+    fi
+    if ! echo "$TARBALL" | grep -Eq "^osg-certificates-[[:digit:]]+\.[[:digit:]]+${SUFFIX}.tar.gz$"; then
+        message "$TARBALL: bad tarball name"
+        message "Extracted from $rpmfile"
+        exit 1
+    fi
+    v=${TARBALL%${SUFFIX}.tar.gz}
+    VERSION_CA=${v#osg-certificates-}
+    SIGFILE=${TARBALL}.sig
+    SIGFILE_URL=${CADISTREPO}/${CADISTREPORELEASETYPE}/${SIGFILE}
+    if ! svn export -q --force "$SIGFILE_URL" "$SIGFILE"; then
+        message "$SIGFILE: unable to download"
+        message "Upstream URL: $SIGFILE_URL"
+        exit 1
+    fi
+    if ! gpg_wrapper --verify "$SIGFILE"; then
+        message "$tarball: GPG verification failed"
+        exit 1
+    fi
 
-        mkdir -p "${CADIR}"
-        mv -f "$TARBALL" "$CATARBALL"
-        mv -f "$SIGFILE" "$CASIGFILE"
-        popd >/dev/null
-        rm -rf $downloaddir
+    CADIR="${TMP}/cadist/${VERSION_CA}${SUFFIX}"
+    CATARBALL="${CADIR}/$TARBALL"
+    CASIGFILE="${CADIR}/$SIGFILE"
 
-        VERSIONFILE_URL=${CADISTREPO}/${CADISTREPORELEASETYPE}/ca-certs-version-${VERSION_CA}${SUFFIX}
-        VERSIONFILE=${TMP}/cadist/ca-certs-version${FILEEXT}
-        if ! svn export -q --force "$VERSIONFILE_URL" "$VERSIONFILE"; then
-            message "$VERSIONFILE: unable to download"
-            message "Upstream URL: $VERSIONFILE_URL"
-            exit 1
-        fi
+    mkdir -p "${CADIR}"
+    mv -f "$TARBALL" "$CATARBALL"
+    mv -f "$SIGFILE" "$CASIGFILE"
+    popd >/dev/null
+    rm -rf $downloaddir
 
-        expected_md5sum=$(
-            perl -lne '/^\s*tarball_md5sum\s*=\s*(\w+)/ and print "$1"' \
-                "$VERSIONFILE")
-        actual_md5sum=$(md5sum "$CATARBALL" | awk '{print $1}')
+    VERSIONFILE_URL=${CADISTREPO}/${CADISTREPORELEASETYPE}/ca-certs-version-${VERSION_CA}${SUFFIX}
+    VERSIONFILE=${TMP}/cadist/ca-certs-version${FILEEXT}
+    if ! svn export -q --force "$VERSIONFILE_URL" "$VERSIONFILE"; then
+        message "$VERSIONFILE: unable to download"
+        message "Upstream URL: $VERSIONFILE_URL"
+        exit 1
+    fi
 
-        if [[ $expected_md5sum != $actual_md5sum ]]; then
-            message "$CATARBALL: md5sum mismatch"
-            message "Expected: $expected_md5sum"
-            message "Actual:   $actual_md5sum"
-            exit 1
-        fi
+    expected_md5sum=$(
+        perl -lne '/^\s*tarball_md5sum\s*=\s*(\w+)/ and print "$1"' \
+            "$VERSIONFILE")
+    actual_md5sum=$(md5sum "$CATARBALL" | awk '{print $1}')
 
-        EXTRACT_FILES="certificates/CHANGES certificates/INDEX.html certificates/INDEX.txt"
-        cd "$CADIR"
+    if [[ $expected_md5sum != $actual_md5sum ]]; then
+        message "$CATARBALL: md5sum mismatch"
+        message "Expected: $expected_md5sum"
+        message "Actual:   $actual_md5sum"
+        exit 1
+    fi
 
-        ## Extract INDEX.txt and CHANGES file; move them appropriately
-        tar --no-same-owner -zxf "${CATARBALL}" -C "$CADIR"
-        mv ${EXTRACT_FILES} "$CADIR"
-        mv certificates/cacerts_md5sum.txt ${TMP}/cadist/cacerts_md5sum${FILEEXT}.txt
-        rm -rf "${CADIR}/certificates/"
+    EXTRACT_FILES="certificates/CHANGES certificates/INDEX.html certificates/INDEX.txt"
+    cd "$CADIR"
 
-        ## Create relevant symlinks including current distro
-        cd ${TMP}/cadist/
-        ln -f -s ${VERSION_CA}${SUFFIX}/CHANGES ${TMP}/cadist/CHANGES
-        ln -f -s ${VERSION_CA}${SUFFIX}/INDEX.txt ${TMP}/cadist/INDEX.txt
-        ln -f -s ${VERSION_CA}${SUFFIX}/INDEX.html ${TMP}/cadist/index.html
+    ## Extract INDEX.txt and CHANGES file; move them appropriately
+    tar --no-same-owner -zxf "${CATARBALL}" -C "$CADIR"
+    mv ${EXTRACT_FILES} "$CADIR"
+    mv certificates/cacerts_md5sum.txt ${TMP}/cadist/cacerts_md5sum${FILEEXT}.txt
+    rm -rf "${CADIR}/certificates/"
 
-        ln -f -s ${VERSION_CA}${SUFFIX}/CHANGES ${TMP}/cadist/CHANGES-${CURRDIR}
-        ln -f -s ${VERSION_CA}${SUFFIX}/INDEX.txt ${TMP}/cadist/INDEX-${CURRDIR}.txt
-        ln -f -s ${VERSION_CA}${SUFFIX}/INDEX.html ${TMP}/cadist/index-${CURRDIR}.html
-        ln -f -n -s ${VERSION_CA}${SUFFIX} ${TMP}/cadist/${CURRDIR}
-        chmod -R ug+rwX ${TMP}/cadist/
-        chmod -R o+rX ${TMP}/cadist/
-        chown ${USER}:goc ${TMP}/cadist/
+    ## Create relevant symlinks including current distro
+    cd ${TMP}/cadist/
+    ln -f -s ${VERSION_CA}${SUFFIX}/CHANGES ${TMP}/cadist/CHANGES
+    ln -f -s ${VERSION_CA}${SUFFIX}/INDEX.txt ${TMP}/cadist/INDEX.txt
+    ln -f -s ${VERSION_CA}${SUFFIX}/INDEX.html ${TMP}/cadist/index.html
 
-        ## Log a new version
-        if [[ ! -d ${CAINSTALL}/${VERSION_CA}${SUFFIX} ]]; then
-            echo "$(date) updated to version ${VERSION_CA}${SUFFIX}" >>${LOGREDIRECTFILENAME}.stdout
-        fi
+    ln -f -s ${VERSION_CA}${SUFFIX}/CHANGES ${TMP}/cadist/CHANGES-${CURRDIR}
+    ln -f -s ${VERSION_CA}${SUFFIX}/INDEX.txt ${TMP}/cadist/INDEX-${CURRDIR}.txt
+    ln -f -s ${VERSION_CA}${SUFFIX}/INDEX.html ${TMP}/cadist/index-${CURRDIR}.html
+    ln -f -n -s ${VERSION_CA}${SUFFIX} ${TMP}/cadist/${CURRDIR}
+    chmod -R ug+rwX ${TMP}/cadist/
+    chmod -R o+rX ${TMP}/cadist/
+    chown ${USER}:goc ${TMP}/cadist/
+
+    ## Log a new version
+    if [[ ! -d ${CAINSTALL}/${VERSION_CA}${SUFFIX} ]]; then
+        echo "$(date) updated to version ${VERSION_CA}${SUFFIX}" >>${LOGREDIRECTFILENAME}.stdout
+    fi
 done
 mkdir -p "$INSTALLBASE"
 rm -rf "$CAINSTALL"
